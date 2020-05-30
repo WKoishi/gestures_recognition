ce44e9bab81f7a2c02737cf8ebb55bbcbd203a43
WKoishi <intelwzt@outlook.com>
2020-05-31 00:02:34 +0800

    综合利用手部的凸起和凹陷

--- gestures_recognition_2.py
+++ gestures_recognition_2.py
@@ -1,8 +1,9 @@
 from cv2 import cv2
-from time import clock
+from time import perf_counter
 from math import acos,pi,log
 import scfun
 import numpy as np
+import matplotlib.pyplot as plt
 
 #创建一个黑框，未使用
 def Create_Outer_Mask(cap):
@@ -18,19 +19,6 @@ def Create_Outer_Mask(cap):
     Outer_mask[row:-row,col:-col]=255
     return Outer_mask
 
-#背景移除
-def Remove_Background(model,frame):
-    fgmask =model.apply(frame)
-    kernel =cv2.getStructuringElement(cv2.MORPH_RECT,(7,7))
-#    kernel_2 =cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
-    kernel_3 =cv2.getStructuringElement(cv2.MORPH_RECT,(9,9))
-    fgmask =cv2.dilate(fgmask,kernel_3,iterations=1)  #膨胀5*5
-    fgmask =cv2.erode(fgmask,kernel,iterations=1)  #腐蚀3*3
-#    fgmask=cv2.morphologyEx(fgmask,cv2.MORPH_CLOSE,kernel_2)
-#    fgmask=cv2.morphologyEx(fgmask,cv2.MORPH_OPEN,kernel)
-#    fgmask=cv2.GaussianBlur(fgmask,(3,3),0)
-    res =cv2.bitwise_and(frame,frame,mask=fgmask)
-    return res
 
 #识别肤色
 def Bodyskin_Detect_Otsu(frame):
@@ -45,12 +33,31 @@ def Bodyskin_Detect_Otsu(frame):
     return skin
 
 
+def Bodyskin_Detect(frame,CrCbHist):
+
+    frame=cv2.blur(frame,(5,5))
+    ycrcb=cv2.cvtColor(frame,cv2.COLOR_BGR2YCrCb)
+    cr=ycrcb[:,:,1]
+    cb=ycrcb[:,:,2]
+
+    result=CrCbHist[cr,cb]  #《这是神句》
+
+    kernel_1=np.ones((3,3),np.uint8)
+    kernel_2=np.ones((5,5),np.uint8)
+    result= cv2.erode(result,kernel_1,iterations=1)
+    result= cv2.dilate(result,kernel_2,iterations=1)
+    #result=cv2.medianBlur(result,3)
+    
+    return result
+
+
 def ConvexHull_Cal(contour):
 
-    def IsTriangle(a,b,c):
-        return a+b>c and a+c>b and b+c>a
+    IsTriangle = lambda a,b,c: a+b>c and a+c>b and b+c>a  #任意两边和必须大于第三边
 
     point_list=[]
+    convex_angle_ls=[]  #凸包角度list
+    concave_angle_ls=[]  #凹陷角度list
     ndefects=0
 
     epsilon = 0.003*cv2.arcLength(contour,True)
@@ -67,18 +74,42 @@ def ConvexHull_Cal(contour):
             far=tuple(contour[f][0])
             point_list.append([sta,far,end])
 
+    #下面的角边标示含义见文件夹里的图片说明
     if len(point_list)>=2:
         for it_1,it_2 in zip(point_list,point_list[1:]+point_list[:1]):
             CA=scfun.Eucledian_Distance(it_1[1],it_1[2])  #far to end
             AB=scfun.Eucledian_Distance(it_1[2],it_2[1])  #end to next far
-            
+            #凸包角度
             if CA+AB>=radius:
-                BC=scfun.Eucledian_Distance(it_1[1],it_2[1])  #far to far，为底边
+                BC=scfun.Eucledian_Distance(it_1[1],it_2[1])  #far to 2nd far，为底边
                 if IsTriangle(CA,AB,BC):
                     angle=acos((CA**2 + AB**2 - BC**2)/(2*CA*AB))
+                    convex_angle_ls.append(angle)
+            #凹陷角度
+            DC=scfun.Eucledian_Distance(it_1[0],it_1[1])  #sta to far
+            if DC+CA>=radius:
+                DA=scfun.Eucledian_Distance(it_1[0],it_1[2])  #sta to end，为底边
+                if IsTriangle(DC,CA,DA):
+                    angle=acos((CA**2 + DC**2 - DA**2)/(2*CA*DC))
+                    concave_angle_ls.append(angle)
 
-                    if angle<=pi/3:
-                        ndefects+=1
+        convex_angle=[x for x in convex_angle_ls if pi/18<=x<=pi/6]  #凸包角度:10度至30度
+        convex_len=len(convex_angle)
+        concave_angle=[x for x in concave_angle_ls if pi/18<=x<=pi/3.5]
+        concave_len=len(concave_angle)
+
+        if convex_len==1 and concave_len==0:
+            ndefects=1
+        elif 1<convex_len<=3 and 0<concave_len<=2:
+            ndefects=concave_len+1
+        elif convex_len>=3 and concave_len==3:
+            mid_angle=[x for x in concave_angle_ls if pi/4 < x <= pi/2.5]  #寻找大拇指与食指间的角度
+            if len(mid_angle)==1:
+                ndefects=5
+            elif len(mid_angle)==0:
+                ndefects=4
+        else:
+            ndefects=0
 
     return ndefects
 
@@ -87,7 +118,7 @@ def ConvexHull_Cal(contour):
 #按轮廓的面积从大到小，检索深度为3
 def Find_Contour(img):
     ndefect_ls=[]
-    MIN_AREA=3000  #检测的轮廓的最小面积
+    MIN_AREA=2000  #检测的轮廓的最小面积
 
     contours,_ =cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     #当len为0时表示没有找到轮廓，当len大于60时表示受到的干扰过大
@@ -114,9 +145,9 @@ def Find_Contour(img):
                 if len(index_list)>=2:
                     right_ls=[ndefect_ls[i] for i in index_list]
                     m_index=ndefect_ls.index(max(right_ls))
-                else:
-                    m_index=0
-            
+                elif len(index_list)==1:
+                    m_index=index_list[0]
+
                 return ndefect_ls[m_index],contours[m_index]
 
     return 0,0
@@ -169,17 +200,25 @@ def main():
     length=30
     ges_num_buf=[0]*length
     
-    cap=cv2.VideoCapture(0)  #启动摄像头
-    model=cv2.createBackgroundSubtractorKNN(history=300,detectShadows=False)  #背景移除KNN模型
+    capture=cv2.VideoCapture(1)  #启动摄像头
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+    
+    skinCrCbHist=np.zeros((256,256),dtype=np.uint8)
+    cv2.ellipse(skinCrCbHist,(113,156),(25,16),43,0,360,255,-1)  #肤色CrCb椭圆模型
     
     while(1):
         
-        _,frame =cap.read()
-        
-        start=clock()
+        _,frame =capture.read()
+
+        start=perf_counter()
         #frame =cv2.bilateralFilter(frame,5,50,100)  #双边滤波
-        xframe =Remove_Background(model,frame)
-        hand =Bodyskin_Detect_Otsu(xframe)
+        #xframe =Remove_Background(model,frame)
+        
+        sta=perf_counter()
+        #hand =Bodyskin_Detect_Otsu(frame)
+        hand=Bodyskin_Detect(frame,skinCrCbHist)
+        fin=perf_counter()
         
         afhand,ges_num,hand_center=Gestures_Detect(hand)
         real_ges_num=Ges_Num_Detect(ges_num,ges_num_buf,length=length)
@@ -194,12 +233,13 @@ def main():
             angle=0
         
         cv2.imshow('koishi',afhand)
-        cv2.imshow('origin',xframe)
+        cv2.imshow('origin',frame)
         
-        finish=clock()
+        finish=perf_counter()
         this_time,last_time=scfun.Filter(finish-start,last_time,rate=0.2)
         print('{:.5f}  {}  {:.5f}  {}  {:.5f}'.format(ges_num, real_ges_num,
                                             finish-start, hand_center, angle))
+        #print(fin-sta)
         
         key=cv2.waitKey(1)&0XFF
         if key == 27:
@@ -207,7 +247,7 @@ def main():
         elif key == ord('q'):
             cv2.imwrite('hand_test.png',hand)
     
-    cap.release()
+    capture.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
