826a2dcee5ea0608271aa1cc44ba2b08541cfe92
WKoishi <intelwzt@outlook.com>
2020-05-31 00:02:56 +0800

    小功能函数

--- scfun.py
+++ scfun.py
@@ -2,6 +2,7 @@
 
 from math import sqrt,atan2,pi,exp
 import numpy as np
+from time import clock
 
 #生成的傅里叶描述子个数
 MIN_DESCRIPTOR = 32
@@ -16,17 +17,9 @@ def Filter(val,last_val,rate):
 def Eucledian_Distance(x,y):
     return sqrt(sum([(a-b)**2 for a,b in zip(x,y)]))
 
-#计算归一化后的傅里叶描述子的欧氏距离
-def Fourier_Descriptor_Similarity(A,B):
-    summary=0
-    for i in range(len(B)):
-        summary+=Eucledian_Distance(A[i],B[i])
-    #summary/=len(B)
-    return summary
-
 #归一化函数
 def Normalization(data):
-    _range = abs(np.max(data))
+    _range = np.max(data)
     return data / _range
 
 #计算方位角
@@ -42,10 +35,36 @@ def Sigmoid(x):
 def Softmax(ls):
     return np.exp(ls)/np.sum(np.exp(ls))
 
+'''
+def Point_Fill(contour):
+    min_dis=4
+    new_cout=[]
+    contour=list(contour)
+    for coor in zip(contour, contour[1:] + contour[:1]):
+        x=coor[0][0]-coor[1][0]
+        y=coor[0][1]-coor[1][1]
+        new_cout.append(list(coor[0]))
+
+        if not y and abs(x)>=2*min_dis:
+            num = abs(int(x/min_dis))-1
+            d= -min_dis if x>0 else min_dis
+            for k in range(num):
+                new_cout.append([coor[0][0]+(k+1)*d, coor[0][1]])
+        
+        elif not x and abs(y)>=2*min_dis:
+            num = abs(int(y/min_dis))-1
+            d= -min_dis if y>0 else min_dis
+            for k in range(num):
+                new_cout.append([coor[0][0], coor[0][1]+(k+1)*d])
+    
+    new_cout=np.array(new_cout)
+    return new_cout
+'''
 
 #根据轮廓计算傅里叶描述子
 #从傅里叶变换结果中取出MIN_DESCRIPTOR个描述点
-def Fourier_Descriptor(contour):
+def Fourier_Descriptor(contour,Normalize=False):
+
     contours_complex = np.empty(contour.shape[:-1], dtype=complex)
     contours_complex.real = contour[:,0]  #横坐标作为实数部分
     contours_complex.imag = contour[:,1]  #纵坐标作为虚数部分
@@ -53,14 +72,15 @@ def Fourier_Descriptor(contour):
 
     if len(fourier_result)>MIN_DESCRIPTOR:
         #截短傅里叶描述子
-        descriptors = np.fft.fftshift(fourier_result)
-        #取中间的MIN_DESCRIPTOR项描述子
-        center_index = int(len(descriptors) / 2)
-        low, high = center_index - int(MIN_DESCRIPTOR / 2), center_index + int(MIN_DESCRIPTOR / 2)
-        descriptors = descriptors[low:high]
-        descriptors = np.fft.ifftshift(descriptors)
+        half=int(MIN_DESCRIPTOR/2)
+        descriptors=np.append(fourier_result[:half],fourier_result[-half:])
     else:
-        return fourier_result
+        descriptors = fourier_result
+
+    if Normalize:
+        M=abs(descriptors[1])
+        reconstruct=[abs(x)/M for x in descriptors[1:-1]]
+        return reconstruct
 
     return descriptors
 
@@ -77,6 +97,7 @@ def Fourier_Contour(descriptors,img_shape):
 
     return reconstruct
 
+'''
 #对傅里叶描述子进行归一化
 def Fourier_Normalization(descriptors):
     M=abs(descriptors[1])
@@ -86,6 +107,6 @@ def Fourier_Normalization(descriptors):
 #    reconstruct=Normalization(reconstruct)
 
     return reconstruct
-
+'''
 
 
